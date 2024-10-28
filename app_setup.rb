ea12152d7f68a721eb08cd828dd7408b61af122d
Ajay Kumar M <ajaybunty91@gmail.com>
2024-10-28 22:16:29 +0530

    Postgres database setup

--- app_setup.rb
+++ app_setup.rb
@@ -1,2 +1,5 @@
 ## App with postgres DB, Tailwind CSS, Javascript esbuild
 rails new chess --database postgresql --css tailwind --javascript esbuild
+
+## Edit credentials.yml file cmd
+$env:EDITOR="code --wait"; rails credentials:edit
