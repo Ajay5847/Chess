037841a7ea6516253f1217355d6e0bd6bd77bffc
Ajay Kumar M <ajaybunty91@gmail.com>
2024-11-03 21:40:51 +0530

    created game class and added user-game associations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,8 @@
 class Game < ApplicationRecord
+  belongs_to :player_one, class_name: User.name
+  belongs_to :player_two, class_name: User.name
+
+  # Validations
+  validates :player_one, presence: true
+  validates :player_two, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-  ## Validations
+  # Associations
+  has_many :games_as_player_one, class_name: Game.name, foreign_key: :player_one_id, dependent: :destroy
+  has_many :games_as_player_two, class_name: Game.name, foreign_key: :player_two_id, dependent: :destroy
+
+  # Validations
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
