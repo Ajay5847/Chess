2691aa463c1bde443fd509cf1cb61eab86bc331b
Ajay Kumar M <ajaybunty91@gmail.com>
2024-11-03 17:20:09 +0530

    remove ,

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: true,
+  ## Validations
+  validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
