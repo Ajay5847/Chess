d65b273b3ac87d0f89c6665db3f930d747679e4f
Ajay Kumar M <ajaybunty91@gmail.com>
2024-11-07 23:41:13 +0530

    Core logic completed

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,4 +5,12 @@ class Game < ApplicationRecord
   # Validations
   validates :player_one, presence: true
   validates :player_two, presence: true
+
+  def chess_game
+    @chess_game ||= Chess::Game.new
+  end
+
+  def make_move(start_pos, end_pos)
+    chess_game.play_turn(start_pos, end_pos)
+  end
 end
