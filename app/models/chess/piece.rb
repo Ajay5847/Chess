df1c8758ef38db37425b6fe7bc0fc6ebdd47ef4a
Ajay Kumar M <ajaybunty91@gmail.com>
2024-11-07 23:21:07 +0530

    These changes will centralise the path_clear? logic

--- app/models/chess/piece.rb
+++ app/models/chess/piece.rb
@@ -16,6 +16,39 @@ module Chess
     end
 
     def path_clear?(start_position, end_position, board)
+      start_x, start_y = start_position
+      end_x, end_y = end_position
+
+      # Check horizontal path (same row)
+      if start_y == end_y
+        range = start_x < end_x ? (start_x + 1...end_x) : (end_x + 1...start_x)
+        range.each do |x|
+          return false if board[x][start_y].present?
+        end
+
+      # Check vertical path (same column)
+      elsif start_x == end_x
+        range = start_y < end_y ? (start_y + 1...end_y) : (end_y + 1...start_y)
+        range.each do |y|
+          return false if board[start_x][y].present?
+        end
+
+      # Check diagonal path
+      elsif (end_x - start_x).abs == (end_y - start_y).abs
+        x_step = end_x > start_x ? 1 : -1
+        y_step = end_y > start_y ? 1 : -1
+
+        current_x = start_x + x_step
+        current_y = start_y + y_step
+
+        while current_x != end_x && current_y != end_y
+          return false if board[current_x][current_y].present?
+          current_x += x_step
+          current_y += y_step
+        end
+      end
+
+      true
     end
   end
 end
--- app/models/chess/rook.rb
+++ app/models/chess/rook.rb
@@ -8,24 +8,5 @@ module Chess
 
       path_clear?(start_position, end_position, board)
     end
-
-    private
-
-    def path_clear?(start_position, end_position, board)
-      start_x, start_y = start_position
-      end_x, end_y = end_position
-
-      # Horizontal Search
-      (start_x..end_x).each do |position|
-        return false if @board[start_x][position].present? && position != end_x
-      end
-
-      # Vertical Search
-      (start_y..end_y).each do |position|
-        return false if @board[position][start_y].present? && position != end_y
-      end
-
-      true
-    end
   end
 end
