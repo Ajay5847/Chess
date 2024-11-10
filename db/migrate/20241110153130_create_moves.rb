e604dd694f5764b356df1409b4a981e5385df475
Ajay Kumar M <ajaybunty91@gmail.com>
2024-11-10 22:24:48 +0530

    Added move history tracking table

---
+++ db/migrate/20241110153130_create_moves.rb
@@ -0,0 +1,14 @@
+class CreateMoves < ActiveRecord::Migration[7.2]
+  def change
+    create_table :moves do |t|
+      t.references :game, null: false, foreign_key: true
+      t.references :player, null: false, foreign_key: true
+      t.string :piece_type
+      t.string :start_position
+      t.string :end_position
+      t.integer :move_number
+
+      t.timestamps
+    end
+  end
+end
