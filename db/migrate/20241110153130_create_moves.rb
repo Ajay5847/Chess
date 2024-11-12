968e46cbc88ec8eab24e31f0a540b099bb9b5dc7
Ajay Kumar M <ajaybunty91@gmail.com>
2024-11-12 22:22:07 +0530

    Migration corrections

--- db/migrate/20241110153130_create_moves.rb
+++ db/migrate/20241110153130_create_moves.rb
@@ -2,7 +2,7 @@ class CreateMoves < ActiveRecord::Migration[7.2]
   def change
     create_table :moves do |t|
       t.references :game, null: false, foreign_key: true
-      t.references :player, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.string :piece_type
       t.string :start_position
       t.string :end_position
--- db/migrate/20241110165736_add_board_state_to_games.rb
+++
@@ -1,6 +0,0 @@
-class AddBoardStateToGames < ActiveRecord::Migration[7.2]
-  def change
-    add_column :games, :board_state, :text
-    add_column :games, :status, :integer, default: 0
-  end
-end
